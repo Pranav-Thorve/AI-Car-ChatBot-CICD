1dfcb9c8d053e896be4fa1207d2e484fb022e7d5
Pranav.Thorve <pranavthorve27@gmail.com>
2025-11-18 10:51:30 -0800

    api chnaged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
 
-  const API_URL = "http://172.17.0.2:9000/infer";
+  const API_URL = "http://host.docker.internal:9000/infer";
 
   const sendMessage = async () => {
     if (!input.trim()) return;
